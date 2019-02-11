f7688dc4b2df278fd1fda38e2eb2147e236a04f1
Richard Lord <lordrichardf@gmail.com>
2019-02-11 00:49:02 -0500

    created callback function to parse url

--- bookScraper/spiders/books.py
+++ bookScraper/spiders/books.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy.http import Request
 
 class BooksSpider(scrapy.Spider):
     name = 'books'
-    allowed_domains = ['http://books.toscrape.com/']
-    start_urls = ['http://books.toscrape.com//']
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['http://books.toscrape.com/']
 
     def parse(self, response):
-        pass
+        urls = response.xpath('//h3/a/@href').extract()
+        for url in urls:
+            absolute_url = response.urljoin(url)
+            yield Request(absolute_url, callback=self.parse_url)
+
+    def parse_url(self, response):
+        title = response.xpath(
+            '//*[@class="col-sm-6 product_main"]/h1/text()').extract_first()
+
+        print(title)
